7a6391ca50788ced2d512e43920228eadbbfc55d
paul lahana <paul.lahana@gmail.com>
2020-04-26 20:56:00 +0200

    go rails tutorial part 1 ok

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  before_action :set_user
+
+  def index
+    redirect_to root_path
+  end
+
+  def show
+    @gifs = @user.gifs.includes(:tags, :user).sorted
+    render "gifs/index"
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
---
+++ app/models/gif.rb
@@ -0,0 +1,13 @@
+class Gif < ApplicationRecord
+  include ImageUploader::Attachment(:image)
+
+  belongs_to :user
+
+  acts_as_taggable
+
+  scope :sorted, -> { order(created_at: :desc) }
+
+  def self.random
+    order("RANDOM()").first
+  end
+end
